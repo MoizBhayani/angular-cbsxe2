d575608b07b44ff4be9f92803ceead805f6c9333
MoizBhayani <moizbhayani@gmail.com>
2019-05-18 14:55:15 +0530

    5 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  message: string = "";
+  userError: any;
+
+  constructor(public fb: FormBuilder, public authService: AuthService, public router: Router) {
+    this.myForm = this.fb.group({
+      email: ['',[Validators.email, Validators.required]],
+      password: ['',[Validators.required]]
+    })
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    this.message="";
+    this.userError = undefined;
+    this.authService.login(form.value.email,form.value.password).then((data)=>{
+      console.log(data);
+      this.message = "You have been logged in Successfully!"
+      this.router.navigate(['/myblogs']);
+    }).catch((error)=>{
+      console.log(error);
+      this.userError = error;
+    })
   }
 
 }
